a2079a1f362c58844f9637d7e42da833b9831558
DrownTheSky <2676293258@qq.com>
2026-03-01 13:10:54 +0800

    add note.sh

---
+++ docs/note.sh
@@ -0,0 +1,17 @@
+# environment
+export ARCH=arm
+export CROSS_COMPILE=arm-none-linux-gnueabihf-
+export PATH=$PATH:/home/weidong/tools/gcc-arm-11.2-2022.02-x86_64-arm-none-linux-gnueabihf/bin
+
+# build
+make distclean
+make mx6ull_14x14_evk_defconfig
+make -j24
+
+# cmd
+printenv
+ping 172.20.10.2
+
+run findfdt
+run netboot
+
